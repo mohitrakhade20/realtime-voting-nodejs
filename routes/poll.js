f77f07401d63653e2ab5abe933598d62185d7e8b
VenomFate-619 <aliamaan119@gmail.com>
2020-08-31 16:46:11 +0530

    form implement

--- routes/poll.js
+++ routes/poll.js
@@ -1,39 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
+const con=require('../controller/poll')
 
-const Vote = require("../models/Vote");
 
-const Pusher = require("pusher");
 
-// const keys = require("../config/keys");
+router.post("/",con.form);
 
-var pusher = new Pusher({
-  appId: keys.pusherAppId,
-  key: keys.pusherKey,
-  secret: keys.pusherSecret,
-  cluster: keys.pusherCluster,
-  encrypted: keys.pusherEncrypted,
-});
+router.route('/poll/:title')
+  .get(con.pollget)
+  .post(con.pollpost)
+// router.post("/poll", (req, res) => {
+//   const newVote = {
+//     os: req.body.os,
+//     points: 1,
+//   };
 
-router.get("/", (req, res) => {
-  Vote.find().then((votes) => res.json({ success: true, votes: votes }));
-});
+//   new Vote(newVote).save().then((vote) => {
+//     pusher.trigger("os-poll", "os-vote", {
+//       points: parseInt(vote.points),
+//       os: vote.os,
+//     });
 
-router.post("/", (req, res) => {
-  const newVote = {
-    os: req.body.os,
-    points: 1,
-  };
-
-  new Vote(newVote).save().then((vote) => {
-    pusher.trigger("os-poll", "os-vote", {
-      points: parseInt(vote.points),
-      os: vote.os,
-    });
-
-    return res.json({ success: true, message: "Thank you for voting" });
-  });
-});
+//     return res.json({ success: true, message: "Thank you for voting" });
+//   });
+// });
 
 module.exports = router;
